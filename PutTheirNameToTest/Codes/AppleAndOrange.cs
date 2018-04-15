43b2fea61601c79dee031e9d5a48ac7214e88413
Vibhor Singh <Vibhor Singh@VIBHORSINGH>
2018-04-15 12:34:17 +0530

    Added New Solution

---
+++ PutTheirNameToTest/Codes/AppleAndOrange.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the countApplesAndOranges function below.
+     */
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+    {
+        /*
+         * Write your code here.
+         */
+        int resApp = 0;
+        int resOr = 0;
+        int appCount = 0;
+        int orCount = 0;
+        for (int i = 0; i <= apples.Length - 1; i++)
+        {
+            resApp = a + (apples[i]);
+            if (resApp >= s && resApp <= t)
+            {
+                appCount++;
+            }
+        }
+        for (int j = 0; j <= oranges.Length - 1; j++)
+        {
+            resOr = b + (oranges[j]);
+            if (resOr >= s && resOr <= t)
+            {
+                orCount++;
+            }
+        }
+        Console.WriteLine(appCount);
+        Console.WriteLine(orCount);
+    }
+
+    static void Main(string[] args)
+    {
+        string[] st = Console.ReadLine().Split(' ');
+
+        int s = Convert.ToInt32(st[0]);
+
+        int t = Convert.ToInt32(st[1]);
+
+        string[] ab = Console.ReadLine().Split(' ');
+
+        int a = Convert.ToInt32(ab[0]);
+
+        int b = Convert.ToInt32(ab[1]);
+
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        int[] apple = Array.ConvertAll(Console.ReadLine().Split(' '), appleTemp => Convert.ToInt32(appleTemp))
+        ;
+
+        int[] orange = Array.ConvertAll(Console.ReadLine().Split(' '), orangeTemp => Convert.ToInt32(orangeTemp))
+        ;
+        countApplesAndOranges(s, t, a, b, apple, orange);
+    }
+}
