e5d50fa3fdbe3f767ed90778641a19c6818e3575
Vibhor Singh <Vibhor Singh@VIBHORSINGH>
2018-04-17 01:51:35 +0530

    Added Implementation Section

---
+++ PutTheirNameToTest/Algorithms/Implementation/DivisibleSumPairs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PutTheirNameToTest.Algorithms.Implementation
+{
+    class DivisibleSumPairs
+    {
+        static int divisibleSumPairs(int n, int k, int[] ar)
+        {
+            // Complete this function
+            int count = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (i < j)
+                    {
+                        if ((ar[i] + ar[j]) % k == 0)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
+        static void Main(String[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = divisibleSumPairs(n, k, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
