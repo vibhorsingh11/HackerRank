d2c1809f8da75e30a83d2abdccd4958e00872c9e
Vibhor Singh <Vibhor Singh@VIBHORSINGH>
2018-05-20 11:53:51 +0530

    Added Data Structures

---
+++ PutTheirNameToTest/Data Structures/Array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PutTheirNameToTest.Data_Structures
+{
+    class Array
+    {
+        static int[] reverseArray(int[] a)
+        {
+            /*
+             * Write your code here.
+             */
+            int[] b = new int[a.Length];
+            int j = 0;
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                b[j] = a[i];
+                j++;
+            }
+            return b;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arrCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int[] res = reverseArray(arr);
+
+            textWriter.WriteLine(string.Join(" ", res));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
